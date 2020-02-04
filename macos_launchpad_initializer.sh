570ac8f9810fadd9468084ad4c20655b8c07a79e
nulLeeKH <i_am@nulleekh.com>
2020-02-04 18:33:38 +0900

    [FIX] Modify the placement of icon to 5&7 in macos_launchpad_initializer.sh

--- macos_launchpad_initializer.sh
+++ macos_launchpad_initializer.sh
@@ -1,5 +1,5 @@
-defaults write com.apple.dock springboard-rows -int 9
-defaults write com.apple.dock springboard-columns -int 9
+defaults write com.apple.dock springboard-rows -int 5
+defaults write com.apple.dock springboard-columns -int 7
 killall Dock
 
 defaults write com.apple.dock ResetLaunchPad -bool true
